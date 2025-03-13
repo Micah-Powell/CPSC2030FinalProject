439de18d30567288f4d31c9bd8aacd5769667c9d
Micah Powell <micahpowell2024@gmail.com>
2025-03-13 10:33:53 -0400

    Added Ticket class and expanded buytik function

--- Project.py
+++ Project.py
@@ -9,28 +9,36 @@ class Customer:
         self.name = name
         self.email = email
 
-    def buytik(self, movie, seats, payment):
-        pass
+    def buytik(self, movie, room, seat, payment):
+        if movie.price > payment:
+            print ("Not sufficient funds")
+            exit(1)
+        else:
+            self.ticket = Ticket(movie, room, seat, movie.price)
+
+class Ticket:
+    def __init__(self, movie, room, seat, price):
+        self.movie = movie.title
+        self.seat = room.seats[f"seat{str(seat)}"].number
+        self.price = price
+        self.desc = f"{self.movie},{self.seat},{self.price}"
         
 class Movie:
-    def __init__(self, title, price):
+    def __init__(self, title, price, room):
         self.title = title
         self.price = price
+        self.room = room
 
 class Room:
     def __init__(self, number):
         self.location = number
-        self.seats = {"Seat"+str(i): Seat(i) for i in range(1,31)}
+        self.seats = {"seat"+str(i): Seat(i) for i in range(1,31)}
 
 class Theater:
     def __init__(self):
         self.rooms = {"room"+str(i): Room(i) for i in range(1,11)}
 
 class Seat:
-    def __init__(self, loc):
-        self.loc = loc
-
+    def __init__(self, number):
+        self.number = number
         
-T = Theater()
-
-print (T.rooms["room1"].seats)
