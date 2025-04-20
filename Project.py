7aa67259b6995e89a3b492487b49c813283b392a
Micah Powell <micahpowell2024@gmail.com>
2025-04-20 17:30:08 -0400

    added comments on all class methods and changed the order of classes to be more coherent

--- Project.py
+++ Project.py
@@ -22,6 +22,7 @@ class Customer(Person):
         A.customers[name] = email
 
     def buytik(self, movie, room, seat, payment):
+        #checks if the payment is sufficient and then creates a ticket based on the information provided and reserves the seat by changing its taken value to True
         if movie.price > payment:
             print ("Not sufficient funds")
             exit(1)
@@ -33,8 +34,14 @@ class Customer(Person):
         T.find_room(room).findseat(seat).buy()
 
     def canceltik(self):
+        #deletes the ticket from the users attributes and returns the seats taken value to False
         T.find_room(self.ticket.room).findseat(self.ticket.seat).cancel()
         del self.ticket
+        
+class AllPeople:
+    def __init__(self):
+        self.customers = {}
+        self.employees = {}
 
 class Ticket:
     def __init__(self, movie, room, seat, price):
@@ -60,6 +67,7 @@ class Ticket:
         return self.__price
     
     def __repr__(self):
+        #changes the display value of printing an object of this class to the string below
         return (f"Movie:{self.movie}, Seat Number:{self.seat}, Ticket Price:{self.price}")
         
 class Movie:
@@ -79,6 +87,20 @@ class Movie:
     @property
     def room(self):
         return self.__room
+    
+class Theater:
+    def __init__(self):
+        self.__rooms = {"room"+str(i): Room(i) for i in range(1,11)}
+
+    def find_room(self, roomnumber):
+        #searches through theater's rooms untill it finds the desired room and returns that object
+        for i in self.rooms:
+            if "room"+str(roomnumber) == i:
+                return self.rooms[i]
+
+    @property
+    def rooms(self):
+        return self.__rooms
 
 class Room:
     def __init__(self, number):
@@ -86,6 +108,7 @@ class Room:
         self.__seats = {"seat"+str(i): Seat(i) for i in range(1,31)}
 
     def findseat(self, seatnumber):
+        #searches through room's seats untill it finds the desired seat and returns that object
         for i in self.seats:
             if "seat"+str(seatnumber) == i:
                 return self.seats[i]
@@ -98,18 +121,6 @@ class Room:
     def seats(self):
         return self.__seats
     
-class Theater:
-    def __init__(self):
-        self.__rooms = {"room"+str(i): Room(i) for i in range(1,11)}
-
-    def find_room(self, roomnumber):
-        for i in self.rooms:
-            if "room"+str(roomnumber) == i:
-                return self.rooms[i]
-
-    @property
-    def rooms(self):
-        return self.__rooms
 
 class Seat:
     def __init__(self, number):
@@ -117,12 +128,15 @@ class Seat:
         self.__taken = False
 
     def __repr__(self):
+        #changes the display value of printing an object of this class to the string below
         return (f"seat {self.number} is taken -->{self.taken}")
 
     def buy(self):
+        #changes the taken value of the seat to True
         self.taken = True
 
     def cancel(self):
+        #changes the taken value of the seat to False
         self.taken = False
 
     @property
@@ -138,10 +152,6 @@ class Seat:
         if type(taken) == bool:
             self.__taken = taken
 
-class AllPeople:
-    def __init__(self):
-        self.customers = {}
-        self.employees = {}
 
         
 A = AllPeople()
